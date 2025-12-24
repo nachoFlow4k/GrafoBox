5150f9bfff689ffbd7e7efd5269c3cf17fc7eb7d
nachoFlow4k <nachoberney@gmail.com>
2025-12-23 23:17:02 -0300

    Probando p5

---
+++ js/canvas.js
@@ -0,0 +1,8 @@
+function setup() {
+  createCanvas(600, 400); // Creates a canvas 600 pixels wide and 400 pixels high
+}
+
+function draw() {
+  background(220); // Sets the background color to a light gray
+  // Your drawing code goes here
+}
