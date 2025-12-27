b65a521a49cc22c0540cd2d778a0c65463db4b30
misProyectosPropios <inakifrutos@outlook.com>
2025-12-27 19:27:03 -0300

    Add testing to the project

---
+++ js/tests/grafo.test.js
@@ -0,0 +1,13 @@
+const sum = require('.././Grafo');
+
+test.only('two plus two is four', () => {
+
+  expect(2 + 2).toBe(4);
+
+});
+/*
+test('adds 1 + 2 to equal 3', () => {
+    grafo = new Grafo(); 
+    expect(grafo.listVertex()).toBe("[]")
+});
+*/
