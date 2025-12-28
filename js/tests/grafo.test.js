c1885436339b5b0af38ed49e3336c107bfec8628
misProyectosPropios <inakifrutos@outlook.com>
2025-12-28 11:04:56 -0300

    Add test 2 to test one vertex name list

--- js/tests/grafo.test.js
+++ js/tests/grafo.test.js
@@ -1,6 +1,14 @@
+
+
 const Grafo = require('.././Grafo');
 
 test('New graph listVertex shoould return an empty list ', () => {
     grafo = new Grafo(); 
     expect(grafo.listVertex()).toBe("[]")
+});
+
+test('New graph listVertex with one vertex should return its name ', () => {
+    grafo = new Grafo(); 
+    grafo.addVertex("v1");
+    expect(grafo.listVertex()).toBe("[v1]")
 });
