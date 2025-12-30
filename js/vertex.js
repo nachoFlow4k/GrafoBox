4154f125a47b29abcac97a8e928de2db5aea94ac
misProyectosPropios <inakifrutos@outlook.com>
2025-12-30 20:51:50 -0300

    Remove unnecesary atributes in vertex class

--- js/vertex.js
+++ js/vertex.js
@@ -5,8 +5,7 @@ class Vertex{
     position;
     color;
     size;
-    edges;
-    conectedVertexes;
+    weigth;
     //nosotros cuando cliqueamos, queremos que se cree un nuevo objeto Vertex que tenga la posicion del mouse
     // y un nombre asignado arbitrariamente para empezar.  El color y tamanio se pueden heredar de una propiedad/regla mas general de la clase grafo.
     constructor(name, coordX, coordY){
@@ -14,8 +13,6 @@ class Vertex{
         this.coordX = coordX
         this.coordY = coordY
         this.position = {x: coordX, y: coordY}
-        this.edges = new Set()
-        this.conectedVertexes = new Set()
     }
 }
 
