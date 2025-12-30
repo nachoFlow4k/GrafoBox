23fcb85ffb55fe683e568fc96ee6e19d95a1ca8f
nachoFlow4k <nachoberney@gmail.com>
2025-12-30 04:33:17 -0300

    agregue class Edge.

--- js/grafo.js
+++ js/grafo.js
@@ -65,14 +65,24 @@ class Vertex{
         this.position = {x: coordX, y: coordY}
         this.edges = new Set()
         this.conectedVertexes = new Set()
+    }
+}
 
+class Edge{
+    id;
+    fromVertex;
+    toVertex;
+    weight;
+    color;
+    size;
+    constructor(fromVertex, toVertex){
+        this.nombre = fromVertex.name + "-" + toVertex.name
+        this.id=(fromVertex,toVertex)
+        this.fromVertex = fromVertex
+        this.toVertex = toVertex
+    
     }
 
     
-
-
-
-
 }
-
 module.exports = Grafo;
