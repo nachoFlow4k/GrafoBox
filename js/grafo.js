4db2c3c7746041004f7a94d38e200ea83d1359a8
nachoFlow4k <nachoberney@gmail.com>
2025-12-30 04:26:56 -0300

    agregue la clase para los nodos

--- js/grafo.js
+++ js/grafo.js
@@ -47,4 +47,32 @@ class Grafo {
     }
 }
 
+class Vertex{
+    name;
+    coordX;
+    coordY;
+    position;
+    color;
+    size;
+    edges;
+    conectedVertexes;
+    //nosotros cuando cliqueamos, queremos que se cree un nuevo objeto Vertex que tenga la posicion del mouse
+    // y un nombre asignado arbitrariamente para empezar.  El color y tamanio se pueden heredar de una propiedad/regla mas general de la clase grafo.
+    constructor(name, coordX, coordY){
+        this.name = name
+        this.coordX = coordX
+        this.coordY = coordY
+        this.position = {x: coordX, y: coordY}
+        this.edges = new Set()
+        this.conectedVertexes = new Set()
+
+    }
+
+    
+
+
+
+
+}
+
 module.exports = Grafo;
