e3be8a7bbdcf78953d08346b4d0736c8bd4974ab
nachoFlow4k <nachoberney@gmail.com>
2026-01-22 15:30:57 -0300

    agregue color negativo

--- js/canvas.js
+++ js/canvas.js
@@ -21,8 +21,9 @@ function drawIcon(imagagePath, item, positionY) {
 
 function draw() {
   background(220);
+  iconNegativo(selectIcon);
   drawIcon(selectIcon, x, 0);
-
+  
   drawIcon(selectIcon, x + 1, 0);
 }
 
@@ -32,4 +33,21 @@ function mouseClicked() {
   circle(mouseX, mouseY, nodeSize);
   console.log(Grafo);
   console.log(graph);
+}
+
+
+function iconNegativo(imagenPath) {  
+    imagenPath.loadPixels();
+
+    for (let i = 0; i < imagenPath.pixels.length; i += 4) {
+        let r = imagenPath.pixels[i];
+        let g = imagenPath.pixels[i + 1];
+        let b = imagenPath.pixels[i + 2];
+
+        imagenPath.pixels[i] = 255-r;   // New R
+        imagenPath.pixels[i + 1] = 255-g; // New G
+        imagenPath.pixels[i + 2] = 255-b; // New B
+    }
+  
+    imagenPath.updatePixels();
 }
